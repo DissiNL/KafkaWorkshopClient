e437fef8bd9334926fde28554ef40fa8107ede5f
Mark Bertels <marinus@backbase.com>
2022-11-08 14:43:44 +0100

    Add solution for step-2

--- src/main/java/com/dissi/kafkaworkshop/kafka/listeners/PetShop.java
+++ src/main/java/com/dissi/kafkaworkshop/kafka/listeners/PetShop.java
@@ -3,6 +3,7 @@ package com.dissi.kafkaworkshop.kafka.listeners;
 
 import static com.dissi.kafkaworkshop.kafka.config.KafkaConstants.TOPIC_PET;
 
+import com.dissi.kafkaworkshop.kafka.model.Pet;
 import lombok.extern.java.Log;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -18,7 +19,7 @@ public class PetShop {
    * @param data The incoming pet data from the kafka topic named "pets".
    */
   @KafkaListener(topics = TOPIC_PET)
-  public void onMessage(@Payload(required = false) String data) {
+  public void onMessage(@Payload(required = false) Pet data) {
     //TODO Add "business logic"
     log.info("Got message from kafka... " + data);
   }
---
+++ src/main/java/com/dissi/kafkaworkshop/kafka/model/Pet.java
@@ -0,0 +1,36 @@
+package com.dissi.kafkaworkshop.kafka.model;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.OffsetDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Pet {
+
+  @JsonProperty("id")
+  private Long id;
+
+  @JsonProperty("createdAt")
+  @DateTimeFormat(iso = ISO.DATE_TIME)
+  private OffsetDateTime createdAt;
+
+  @JsonProperty("name")
+  private String name;
+
+  @JsonProperty("type")
+  private String type;
+
+  @JsonProperty("owner")
+  private String owner;
+}
